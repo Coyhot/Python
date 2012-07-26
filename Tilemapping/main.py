7713c6412f8c6e7c6f3d4c88a8d1a3e50433f31d
Coyhot <l3coyott3@gmail.com>
2012-07-26 14:22:04 +0300

    Removed debugging utilities

--- Tilemapping/main.py
+++ Tilemapping/main.py
@@ -8,7 +8,6 @@ from data import *
 window = pygame.display.set_mode((windowWidth,windowHeight))
 tileset = pygame.image.load(tilesetPath).convert()
 for row in range(xTiles):
-    print "\n"
     for tile in range(yTiles):
         try:
             window.blit(tileset,(tile*tileWidth,row*tileHeight),tiles[int(world[row][tile])//len(tiles[0])][int(world[row][tile])-(int(world[row][tile])//len(tiles[0]))*len(tiles[0])])

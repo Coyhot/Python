d6b565c5eeae620585826008bab9ef6d3edaf11e
Coyhot <l3coyott3@gmail.Com>
2012-08-01 16:30:57 +0200

    Did some work on the editor. Actually thinking on how to choose the tile we want to place ( not so easy )

--- editor/data.py
+++ editor/data.py
@@ -6,11 +6,10 @@ Contains all the data that the main file needs to perform the tilemapping
 Created by coyhot
 19/07/12 - */*/*
 """
-tilesetPath = "files/tileset2.png"
 tileHeight,tileWidth = 16,16 #Size of the tiles
 xTiles,yTiles = 15,15 #Size of the level
 windowHeight, windowWidth = tileHeight*xTiles,tileWidth*yTiles #*PROVISOIR* Window size from tiles
-tilesetWidth,tilesetHeight = 42,24 #Number of tiles in the tileset
+tilesetWidth,tilesetHeight = 8,6 #Number of tiles in the tileset
 tileEspacement = 2 #The space in pixel between tiles
 
 
@@ -31,18 +30,31 @@ class Level:
         self.width = width
         self.tileset = tileset
         self.basetile = basetile
-        self.level = []
         self.generateBaseArray()
-        
+    
     def generateBaseArray(self):
+        self.level = []
         for a in range(self.height):
             self.level.append([])
             for b in range(self.width):
                 self.level[a].append(self.basetile)
+    
     def getTile(self,x,y):
         return self.level[x][y]
+    
+    def setTile(self,x,y,number):
+        self.level[x][y] = number
     def printTTY(self):
         print self.level
+    
+    def display(self,surface):
+        for row in range(xTiles):
+            for tile in range(yTiles):
+                self.tileNumber = self.getTile(row,tile)
+                
+                self.xTilePos, self.yTilePos = self.tileNumber // tilesetWidth , self.tileNumber % tilesetWidth
+                surface.blit(self.tileset,(tile*tileWidth,row*tileHeight),tiles[self.xTilePos][self.yTilePos])
         
+
 
 
---
+++ editor/editorData.py
@@ -0,0 +1,6 @@
+from data import *
+tilesetPath = "files/tilesetmini.png"
+tileToPlace = 2
+tilePosInGrid = [tileToPlace//tilesetWidth,tileToPlace%tilesetHeight]
+actualTile = 9
+
